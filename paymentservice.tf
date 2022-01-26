0ced433867207029e1304c8905d06ded5ea081c6
tobi5775 <tobias.wittmann96@web.de>
2022-01-26 16:38:32 +0100

    add paymentservice

---
+++ paymentservice.tf
@@ -0,0 +1,83 @@
+module "paymentservice-application" {
+  source = "./modules/elb-postgres-application"
+
+  application_name = "paymentservice"
+  s3_bucket_id     = var.s3_bucket_id
+  environment      = var.environment
+  db_instance_type = "db.t2.micro"
+  instance_type    = "t2.micro"
+  public_subnets   = module.vpc.public_subnets
+  private_subnets  = module.vpc.private_subnets
+  vpc_id           = module.vpc.vpc_id
+  region           = var.region
+  aws_access_key   = var.aws_access_key
+  aws_secret_key   = var.aws_secret_key
+}
+
+###############
+# API_Gateway #
+###############
+resource "aws_api_gateway_resource" "payment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.service.id
+  path_part   = "payment"
+}
+
+resource "aws_api_gateway_resource" "payment_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.payment.id
+  path_part   = "api"
+}
+
+resource "aws_api_gateway_resource" "payment_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.payment_api_resource.id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "payment_get" {
+  rest_api_id        = aws_api_gateway_rest_api.api.id
+  resource_id        = aws_api_gateway_resource.payment_proxy.id
+  http_method        = "GET"
+  authorization      = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_method" "payment_post" {
+  rest_api_id        = aws_api_gateway_rest_api.api.id
+  resource_id        = aws_api_gateway_resource.payment_proxy.id
+  http_method        = "POST"
+  authorization      = "COGNITO_USER_POOLS"
+  authorizer_id      = aws_api_gateway_authorizer.api_authorizer.id
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "payment_get_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.payment_proxy.id
+  http_method             = aws_api_gateway_method.payment_get.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${module.paymentservice-application.elb_endpoint_url}/api/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
+resource "aws_api_gateway_integration" "payment_post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.payment_proxy.id
+  http_method             = aws_api_gateway_method.payment_post.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${module.paymentservice-application.elb_endpoint_url}/api/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
