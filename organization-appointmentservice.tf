17e8206c3cef8c8693134d69583feb2efa05dea1
tobi5775 <tobias.wittmann96@web.de>
2022-04-04 18:40:52 +0200

    fix resource reference

--- organization-appointmentservice.tf
+++ organization-appointmentservice.tf
@@ -67,7 +67,7 @@ resource "aws_api_gateway_resource" "organizationappointment_slots" {
 
 resource "aws_api_gateway_method" "organizationappointment_slots" {
   rest_api_id        = aws_api_gateway_rest_api.api.id
-  resource_id        = aws_api_gateway_resource.organizationappointment_proxy.id
+  resource_id        = aws_api_gateway_resource.organizationappointment_slots.id
   http_method        = "GET"
   authorization      = "NONE"
   request_parameters = {
